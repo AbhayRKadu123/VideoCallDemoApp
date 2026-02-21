8815c8baf4e0677ec8a57af0ae67dbfdb703e7fd
Abhay Kadu <abhay@triplewsols.com>
2026-02-21 12:15:11 +0530

    newcommit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+
+    socket.to(roomId).emit("user-joined");
+
+    socket.on("offer", (offer) => {
+      socket.to(roomId).emit("offer", offer);
+    });
+
+    socket.on("answer", (answer) => {
+      socket.to(roomId).emit("answer", answer);
+    });
+
+    socket.on("ice-candidate", (candidate) => {
+      socket.to(roomId).emit("ice-candidate", candidate);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
